b5e0a89fbc4f83949f5f28135c511dbbc68d2e82
Christopher Baker <christopher.ray.baker@gmail.com>
2017-12-08 08:38:02 -0600

    Added description
    Added description and minor output for tracking current user

--- Change-Folder-Permissions-Bulk.ps1
+++ Change-Folder-Permissions-Bulk.ps1
@@ -1,8 +1,14 @@
-
+#############################################
+# Use to assign User full rights to a folder named after their UserID
+# i.e. chrisb would be my UserID and personal folder name
+# Each User has a folder in this location
+# This will update the User's folder itself plus everyfile therein
+###################################
 $startingDir = "\\path with each user folder"
 $Right="FullControl"
 
 foreach($user in $(Get-ChildItem $startingDir)){
+    Write-Host "Updating permissions for User $user ..."
     $Principal="domain\$user"
     $rule=New-Object System.Security.AccessControl.FileSystemAccessRule($Principal, $Right, "Allow")
 
